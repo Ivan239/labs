c7d8ee6ebd5406915f3d73bd1fdc944c21a28408
Ivan239 <ivan-obraztsov@ya.ru>
2022-12-10 15:11:28 +0300

    Add ansible for docker installation

--- terraform/learn-terraform-aws-instance/main.tf
+++ terraform/learn-terraform-aws-instance/main.tf
@@ -14,10 +14,37 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
+  ami           = "ami-0ecc74eca1d66d8a6"
   instance_type = "t2.micro"
+  key_name = aws_key_pair.deploy_key.key_name
+  security_groups = [aws_security_group.app_sg.name]
 
   tags = {
     Name = var.instance_name
+  }
+}
+
+resource "aws_key_pair" "deploy_key" {
+  key_name   = "deploy_key"
+  public_key = var.deploy_public_key
+}
+
+resource "aws_security_group" "app_sg" {
+  name = "app_sg"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }

21a718bcaf84a2e25176958b70843de5815b2d17
Ivan239 <ivan-obraztsov@ya.ru>
2022-12-12 15:25:48 +0300

    Add ConfigMaps

--- app_python/moscowtime/moscowtime/time_page/views.py
+++ app_python/moscowtime/moscowtime/time_page/views.py
@@ -2,6 +2,12 @@ import zoneinfo
 from datetime import datetime
 
 from django.http import HttpResponse
+from fastapi import FastAPI
+
+from app_python.moscowtime.internal_storage import Store
+
+app = FastAPI()
+store = Store()
 
 
 def homePageView(request):
@@ -10,4 +16,11 @@ def homePageView(request):
 
 def moscowTime():
     zone = zoneinfo.ZoneInfo("Europe/Moscow")
-    return datetime.now(zone).strftime("Current time in moscow is %H:%M:%S")
+    date = datetime.now(zone).strftime("Current time in moscow is %H:%M:%S")
+    store.add_visit(date)
+    return date
+
+
+@app.get("/visits")
+async def return_visits():
+    return store.get_visits()
